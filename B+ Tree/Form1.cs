ad42e4cfc0341babb3a9cb0b606bdb56d2649c8d
Gerry Surya <ger_26_6@hotmail.com>
2017-05-27 14:16:45 +0700

    copas word

--- B+ Tree/Form1.cs
+++ B+ Tree/Form1.cs
@@ -21,6 +21,8 @@ namespace B__Tree
             InitializeComponent();
         }
 
+        int m = 20;
+
         class MULTINODE
         {
             public int n, size;
@@ -39,8 +41,126 @@ namespace B__Tree
                 this.next = null;
                 this.parent = null;
             }
+
+            public int insert(int key)
+            {
+                if (n == 0)
+                {
+                    keys[0] = key;
+                    children[1] = null;
+                    n++;
+                    return 0;
+                }
+                else
+                {
+                    if (key > keys[size - 1])
+                    {
+                        keys[n++] = key;
+                        children[n] = null;
+                        return n - 1;
+                    }
+                    else
+                    {
+                        int position = find_position(key);
+                        for (int i = n; i > position; i--)
+                        {
+                            children[i + 1] = children[i];
+                            keys[i] = keys[i - 1];
+                        }
+                        keys[position] = key;
+                        children[position + 1] = null;
+                        n++;
+                        return position;
+                    }
+                }
+            }
+            public int find_position(int key)
+            {
+                int position = Array.BinarySearch(keys, 0, n, key);
+                if (position < 0)
+                    return ~position;
+                else if (position < n && keys[position] == key)
+                    return position + 1;
+                else
+                    return position;
+            }
+
         }
 
+        void insert(ref MULTINODE root, int key)
+        {
+            MULTINODE new_node = new MULTINODE(m);
+            if (root == null)
+            {
+                root = new_node;
+                root.insert(key);
+            }
+            else
+            {
+                MULTINODE current = root;
+                while (true)
+                {
+                    if (current.children[0] == null)
+                    { //insert must be in leaf
+                        int position = current.insert(key);
+                        while (current.n > current.size)
+                        { //current is overflow
+                          //split by two
+                            Object[] result = split(ref root, current);
+                            //try to insert middle key to parent
+                            MULTINODE new_node1 = (MULTINODE)result[0];
+                            key = (int)result[1];
+                            current = current.parent;
+                            position = current.insert(key);
+                            current.children[position + 1] = new_node1;
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        //interior node must have at least two children
+                        int position = current.find_position(key);
+                        current = current.children[position];
+                    }
+                }
+            }
+        }
+        Object[] split(ref MULTINODE root, MULTINODE node)
+        {
+            int mid = node.n / 2;
+            if (node.parent == null)
+            {
+                root = new MULTINODE(m);
+                root.children[0] = node;
+                node.parent = root;
+            }
+            MULTINODE new_node = new MULTINODE(m);
+            new_node.parent = node.parent;
+            int nk = 0;
+            for (int k = (node.children[0] == null /*leaf*/) ? mid : mid + 1;
+                 k < node.n; k++, nk++)
+            {
+                new_node.insert(node.keys[k]);
+                move_parent(new_node, nk, node.children[k]);
+            }
+            move_parent(new_node, nk, node.children[node.n]);
+            node.n = mid;
+            if (node.children[0] == null)
+            { //leaf
+                new_node.next = node.next;
+                if (node.next != null)
+                    node.next.prev = new_node;
+                new_node.prev = node;
+                node.next = new_node;
+            }
+            return new Object[] { new_node, node.keys[mid] };
+        }
+        void move_parent(MULTINODE parent, int position, MULTINODE child)
+        {
+            parent.children[position] = child;
+            if (child != null)
+                child.parent = parent;
+        }
 
 
         private void Form1_Load(object sender, EventArgs e)
